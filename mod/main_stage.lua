29cf6d276ec2276f3f462b9070e62ac556c49aad
yao.peng <yao.peng@happyelements.com>
2026-09-22 17:31:41 +0800

    th31 re2

--- mod/main_stage.lua
+++ mod/main_stage.lua
@@ -1180,15 +1180,13 @@ NewStage("TH31", "永夜LastWord", 29, function(self)
     self:Option(TH08_bg, "TH08_NEW_0")
     self:Task(function()
         task.Wait(60)
-        for i = 1, 7 do
+        ---17 张 Last Word 一张一张来（boss.CreateGroup 会阻塞到这张卡打完）。
+        ---原来分成 1..7 / 8..17 两段没有意义；真正的坑是旧版把 17 个角色全
+        ---登记在组 1 的 a..q 上，CreateGroup(1) 会一次把 17 个 boss 全建出来。
+        for i = 1, 17 do
             boss.CreateGroup(i, self.level)
             task.Wait(60)
         end
-        for i = 8, 17 do
-            boss.CreateGroup(i, self.level)
-            task.Wait(60)
-        end
-        task.Wait(60)
     end)
     self:Next(52)
 end)--29
